b7a8d8f0ad0ab6953a644517e1a5fa9a88a5f5f3
Youva Chaou <youvachaou1234@gmail.com>
2023-10-02 13:00:46 -0400

    dsds

--- src/stage/components/StageItem.jsx
+++ src/stage/components/StageItem.jsx
@@ -8,7 +8,7 @@ const StageItem = props => {
       <div className='stage-item__actions'>
       <h1>{props.title}</h1>
       <h2>{props.description}</h2>
-      <h2>{props.salary}</h2>
+      <h2>{props.salary}$</h2>
       <h2>{props.address}</h2>
       <h2>{props.startingDate}</h2>
       <h2>{props.endingDate}</h2>
