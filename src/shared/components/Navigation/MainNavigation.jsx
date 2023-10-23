624665e5ae038696ca9bd01ec9964e52c5cea47a
Youva Chaou <youvachaou1234@gmail.com>
2023-10-23 16:53:14 -0400

    مخمخمخخخسشتخهشسايساههشسهخيهخشسهيخسش

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 //import Backdrop from '../UIElements/Backdrop';
-import './MainNavigation.css';
+import "./MainNavigation.css";
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
   //const [setDrawerIsOpen] = useState(false);
 
   /*const openDrawerHandler = () => {
@@ -16,20 +16,15 @@ const MainNavigation = props => {
   };
   */
 
-
-
   return (
     <React.Fragment>
-      
-
-
       <MainHeader>
-
+       
         <h1 className="main-navigation__title">
           <Link to="/">Collège Montmorency</Link>
         </h1>
         <nav className="main-navigation__header-nav">
-          <NavLinks />
+          <NavLinks utilisateur={props.utilisateur}/>
         </nav>
       </MainHeader>
     </React.Fragment>
--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
 
-import { AuthContext } from '../../context/auth-context';
-import './NavLinks.css';
+import { AuthContext } from "../../context/auth-context";
+import "./NavLinks.css";
 
-const NavLinks = props => {
+const NavLinks = (props) => {
   const auth = useContext(AuthContext);
 
   return (
@@ -14,15 +14,27 @@ const NavLinks = props => {
           Accueil
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/creer-compte">Créer un comptet</NavLink>
-      </li>
-      <li>
-        <NavLink to="/connexion">Se connecter</NavLink>
-      </li>
-      <li>
-        <NavLink to="stage">Stage</NavLink>
-      </li>
+      {props.utilisateur.message === "student object" ||
+      props.utilisateur.message === "employer object" ? (
+        <>
+          {props.utilisateur.message === "student object"
+            ? props.utilisateur.student.lastName + " est connecté (étudiant)"
+            : props.utilisateur.employer.lastName + " est connecté (employeur)"}
+          <li>
+            <NavLink to="stage">Stage</NavLink>
+          </li>
+          <li></li>
+        </>
+      ) : (
+        <>
+          <li>
+            <NavLink to="/creer-compte">Créer un comptet</NavLink>
+          </li>
+          <li>
+            <NavLink to="/connexion">Se connecter</NavLink>
+          </li>
+        </>
+      )}
     </ul>
   );
 };
