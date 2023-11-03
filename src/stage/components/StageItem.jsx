e5c92d46da8a47eae4226900e4bbc2b0d59f5eb8
Médéric Bélec <mede.hockey@gmail.com>
2023-11-03 16:50:50 -0400

    FIN

--- src/stage/components/StageItem.jsx
+++ src/stage/components/StageItem.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
 import { useForm } from "../../shared/hooks/form-hook";
-import './StageItem.css';
+import "./StageItem.css";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
@@ -11,7 +11,7 @@ import {
   VALIDATOR_NUMBER,
 } from "../../shared/util/validators";
 
-const StageItem = props => {
+const StageItem = (props) => {
   const [isEditing, setIsEditing] = useState(false);
   const [modifiedValues, setModifiedValues] = useState({
     title: props.title,
@@ -75,7 +75,7 @@ const StageItem = props => {
     setIsEditing(false);
   };
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
     setModifiedValues({ ...modifiedValues, [name]: value });
   };
@@ -90,83 +90,72 @@ const StageItem = props => {
           <h2>{props.address}</h2>
           <h2>{formatDate(props.startingDate)}</h2>
           <h2>{formatDate(props.endingDate)}</h2>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <h1><u>{props.title}</u></h1>
-          <br />
-          <h2>{props.description}</h2>
-          <h2>Salaire: {props.salary}$</h2>
-          <h2>Adresse: {props.address}</h2>
-          <h2>Date du Debut: {formatDate(props.startingDate)}</h2>
-          <h2>Date de Fin: {formatDate(props.endingDate)}</h2>
-          {(props.isStudent) ? <></> : <>
-          <Button onClick={props.onClickDeleteFunction}>Supprimer</Button>
-          <Button onClick={handleModifyClick}>Modifier</Button></>}
-        </React.Fragment>
-      )}
-      {isEditing && (
-        <React.Fragment>
-          <Input
+
+          <label htmlFor="title">Titre du stage</label>
+          <input
             id="title"
             element="input"
             type="text"
-            label="Titre du stage"
             validators={[VALIDATOR_REQUIRE()]}
             name="title"
             value={modifiedValues.title}
             onChange={handleInputChange}
             onInput={inputHandler}
           />
+          <br></br>
+          <label htmlFor="description">Description</label>
           <input
             id="description"
             element="input"
             type="text"
-            label="Description"
             validators={[VALIDATOR_REQUIRE()]}
             name="description"
             value={modifiedValues.description}
             onChange={handleInputChange}
             onInput={inputHandler}
           />
+          <br></br>
+          <label htmlFor="salary">Salaire</label>
           <input
             id="salary"
             element="input"
             type="text"
-            label="Salaire"
             validators={[VALIDATOR_REQUIRE()]}
             name="salary"
             value={modifiedValues.salary}
             onChange={handleInputChange}
             onInput={inputHandler}
           />
+          <br></br>
+          <label htmlFor="address">Adresse de l'entreprise</label>
           <input
             id="address"
             element="input"
             type="text"
-            label="Adresse de l'entreprise"
             validators={[VALIDATOR_REQUIRE()]}
             name="address"
             value={modifiedValues.address}
             onChange={handleInputChange}
             onInput={inputHandler}
           />
+          <br></br>
+          <label htmlFor="startingDate">Date de début</label>
           <input
             id="startingDate"
             element="input"
             type="date"
-            label="Date de début"
             validators={[VALIDATOR_REQUIRE()]}
             name="startingDate"
             value={formatDate(modifiedValues.startingDate)}
             onChange={handleInputChange}
             onInput={inputHandler}
           />
+          <br></br>
+          <label htmlFor="endingDate">Date de fin</label>
           <input
             id="endingDate"
             element="input"
             type="date"
-            label="Date de fin"
             validators={[VALIDATOR_REQUIRE()]}
             name="endingDate"
             value={formatDate(modifiedValues.endingDate)}
@@ -174,10 +163,15 @@ const StageItem = props => {
             onInput={inputHandler}
           />
           <div className="button-row">
-            {(props.isStudent) ? <></> : <><Button onClick={handleInputChange}>Enregistrer</Button>
-            <Button onClick={handleCancelClick}>Annuler</Button>
-            <Button onClick={props.onClickDeleteFunction}>Supprimer</Button></>}
-            
+            {props.isStudent ? (
+              <></>
+            ) : (
+              <>
+                <Button onClick={handleSaveClick}>Enregistrer</Button>
+                <Button onClick={handleCancelClick}>Annuler</Button>
+                <Button onClick={props.onClickDeleteFunction}>Supprimer</Button>
+              </>
+            )}
           </div>
         </React.Fragment>
       ) : (
@@ -188,8 +182,18 @@ const StageItem = props => {
           <h2>{props.address}</h2>
           <h2>{formatDate(props.startingDate)}</h2>
           <h2>{formatDate(props.endingDate)}</h2>
-          <Button onClick={props.onClickDeleteFunction}>Supprimer</Button>
-          <Button onClick={handleModifyClick}>Modifier</Button>
+          <div className="button-row">
+            {props.isStudent ? (
+              <></>
+            ) : (
+              <>
+                <Button onClick={props.onClickDeleteFunction}>
+                  Supprimer{" "}
+                </Button>
+                <Button onClick={handleModifyClick}>Modifier</Button>
+              </>
+            )}
+          </div>
         </React.Fragment>
       )}
     </div>
